8f217a788a70e7eee12644347bf8276e18ad3141
Gustavo Silva <xgustavossilva@gmail.com>
2020-04-20 02:51:20 -0300

    New: Added victory screen

--- GameManager.gd
+++ GameManager.gd
@@ -20,13 +20,13 @@ func set_player_life(value):
 	playerLife = clamp(value, 0, max_life)
 	if playerLife <= 0:
 		game_over()
-	if level > 7 && playerLife > 0:
+	if level >= 8 && playerLife > 0:
 		victory()
 	
 func resetValues():
 	level = 1
 	spawn_time = 3
-	velocity = 10
+	velocity = 20
 	playerLife = 100	
 
 func game_over():
@@ -34,5 +34,6 @@ func game_over():
 	get_tree().change_scene("res://GameOver.tscn")
 	
 func victory():
-	print("victory")
+	resetValues()
+	get_tree().change_scene("res://Victory.tscn")
 
